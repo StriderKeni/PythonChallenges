d820a6786977049f820da9c5bec2bbe9a08d98a8
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-06-12 18:41:49 -0400

    Add http://hr.gs/u06

---
+++ rotate_arr.py
@@ -0,0 +1,15 @@
+
+
+def rotLeft(a, d):
+    return a[d:]+a[:d]
+
+
+if __name__ == "__main__":
+    nd = input().split()
+
+    n = int(nd[0])
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    print(rotLeft(a, d))
