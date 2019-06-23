b75e69ff1c2cd5bf6f3aabc16bc0b625f2eb52a2
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-06-22 21:45:57 -0400

    Update problem using tuple and packing

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,16 @@
 def countSwaps(a):
-    temp = 0
     swaps = 0
     while a != sorted(a):
         for i in range(len(a)-1):
             if a[i] > a[i+1]:
-                temp = a[i+1]
-                a[i+1] = a[i]
-                a[i] = temp
+                a[i], a[i+1] = a[i+1], a[i]
                 swaps += 1
             else:
                 continue
-    return swaps, a[0], a[-1]
+
+    print("Array is sorted in {} swaps.".format(swaps))
+    print("First Element: {}".format(a[0]))
+    print("Last Element: {}".format(a[-1]))
 
 
 if __name__ == "__main__":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     n = int(input())
 
     a = list(map(int, input().rstrip().split()))
-    print(countSwaps(a))
+    countSwaps(a)
